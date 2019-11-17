1b1b3aac337aedbc50f6e5a7a3ea3fccc3d262a8
Mateusz Krolikowski <krolikmat@gmail.com>
2019-11-17 23:38:45 +0100

    weather app done

--- index.js
+++ index.js
@@ -1,40 +1,47 @@
-console.log("hello world");
-
 window.addEventListener("load", () => {
   let lat;
   let lon;
   const weatherTemperature = document.querySelector(".temperature");
   const weatherDescription = document.querySelector(".description");
+  const iconElement = document.querySelector(".icon");
+  const errorMsg = document.querySelector(".error-msg");
 
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(position => {
-      lat = position.coords.latitude;
-      lon = position.coords.longitude;
-      let weatherApi = `https://api.darksky.net/forecast/3624b3f59e055e002b7371bc12fb5983/${lat},${lon}`;
+  navigator.geolocation.getCurrentPosition(position => {
+    lat = position.coords.latitude;
+    lon = position.coords.longitude;
+    let weatherApi = `https://api.darksky.net/forecast/3624b3f59e055e002b7371bc12fb5983/${lat},${lon}`;
+    // let airQualityApi = `https://api.waqi.info/feed/geo:${lat};${lon}/?token=25043f9679b1359396e1a0704780b1304a866021`; will be added in future
 
-      let airQualityApi = `https://api.waqi.info/feed/geo:${lat};${lon}/?token=25043f9679b1359396e1a0704780b1304a866021`;
-      fetch(weatherApi)
-        .then(res => {
-          return res.json();
-        })
-        .then(data => {
-          console.log(data);
-          const { icon, summary, temperature } = data.currently;
-          const celsius = Math.floor((temperature - 30) / 2);
-          weatherTemperature.innerText = celsius;
-          weatherDescription.innerText = summary;
-          setIcons(icon, document.querySelector(".icon"));
-        });
-    });
-  }
+    fetch(weatherApi)
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        const { icon, summary, temperature } = data.currently;
+        const celsius = Math.floor((temperature - 30) / 2);
+        weatherTemperature.innerText = celsius;
+        weatherDescription.innerText = summary;
+        setIcons(icon, iconElement);
+      })
+  },
+    function error() {
+      if (error.code == error.PERMISION_DENIED)
+        iconElement.style.display = "none";
+      errorMsg.style.display = "block";
+    }
+  );
+
   const setIcons = (icon, iconId) => {
     const skycons = new Skycons({ color: "white" });
     const currentIcon = icon.replace(/-/g, "_").toUpperCase();
     skycons.play();
     return skycons.set(iconId, Skycons[currentIcon]);
   };
+
   getScreenSize();
-  // setWallpaper();
+  setWallpaper();
+  getCurrentDate();
+
 });
 
 let screenWidth;
@@ -59,3 +66,9 @@ const getCurrentTime = setInterval(() => {
   const clock = document.querySelector(".clock");
   clock.innerText = `${hours}:${minutes}:${seconds}`;
 }, 1000);
+
+const getCurrentDate = () => {
+  let date = new Date();
+  const dateElement = document.querySelector('.date')
+  dateElement.innerText = date.toLocaleDateString();
+};
